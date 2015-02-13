4e9cb2125431e31dfc88df36763a7fda40c3f4c1
Mattia Corvaglia <corvagliamattia@gmail.com>
2015-02-13 11:20:41 +0100

    Clean code

---
+++ app/src/main/java/com/mcdev/passbox/views/CustomDialogPreference.java
@@ -0,0 +1,18 @@
+package com.mcdev.passbox.views;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+/**
+ * Custom DialogPreference
+ * @author Mattia Corvaglia
+ */
+public class CustomDialogPreference extends DialogPreference {
+
+    // Constructor
+    public CustomDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    
+}
